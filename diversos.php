755e182e6fb2d186958300a5865d5dbd53a3a443
Rafaella Rodrigues <112015111+JuliaLabre@users.noreply.github.com>
2022-11-23 15:55:09 -0300

    Conteudos

---
+++ diversos.php
@@ -0,0 +1,53 @@
+<?php
+require 'header.php'
+?>
+  <!-- Conteúdo -->
+  <div class="row">
+    <div class="col-md-3">
+      <div class="card bg-light mb-3" style="max-width: 18rem;">
+        <img class="card-img-top" src="imagens/diversos1.webp" alt="Imagem de capa do card">
+        <div class="card-body">
+          <h5 class="card-title">Garrafa Inox</h5>
+          <p class="card-text">Lindos modelos de garrafas inox</p>
+          <a href="#" class="btn btn-primary">Comprar</a>
+        </div>
+      </div>
+    </div>
+      <div class="col-md-3">
+        <div class="card bg-light mb-3">
+          <img class="card-img-top" src="imagens/diversos2.webp" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title">Garrafa Preta</h5>
+            <p class="card-text">Linda garrafa preta.</p>
+            <a href="#" class="btn btn-primary">Comprar</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card bg-light mb-3">
+          <img class="card-img-top" src="imagens/diversos3.webp" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title">Garrafas Coloridas</h5>
+            <p class="card-text">Modelos especiais.</p>
+            <a href="#" class="btn btn-primary">Comprar</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card bg-light mb-3">
+          <img class="card-img-top" src="imagens/diversos4.webp" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title">Garrafa transparente</h5>
+            <p class="card-text">Estilo clean.</p>
+            <a href="#" class="btn btn-primary">Comprar</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Footer -->
+  <?php
+require 'footer.php'
+?>
---
+++ footer.php
@@ -0,0 +1,62 @@
+<?php
+echo <<<HTML
+
+<div class="container-fluid">
+    <div class="row text-center bg-light">
+      <div class="col-md-3">
+        <h4>Institucional</h4>
+        <p><a class="navbar-link" href="#">Home</a></p>
+        <p><a class="navbar-link" href="#">Nossa Academia</a></p>
+        <p><a class="navbar-link" href="#">Conheça os Planos</a></p>
+        <p><a class="navbar-link" href="#">Área do Cliente</a></p>
+
+      </div>
+      <div class="col-md-3">
+        <h4>Atividades</h4>
+        <ul class="navbar-nav">
+          <li class="navbar-item"><a class="navbar-link" href="#">Spinning</a></li>
+          <li class="navbar-item"><a class="navbar-link" href="#">Jump</a></li>
+          <li class="navbar-item"><a class="navbar-link" href="#">Hidroginástica</a></li>
+          <li class="navbar-item"><a class="navbar-link" href="#">Pilates</a></li>
+        </ul>
+      </div>
+
+      <div class="col-md-3">
+        <h4>Funcionamento</h4>
+        <p>Segunda à Sábado - 06:00 às 23:00</p>
+        <p>Domingo - 07:00 às 13:00</p>
+      </div>
+
+      <div class="col-md-3 mapa">
+        <h4>Localização</h4>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14701.424874456801!2d-43.5588465!3d-22.9002284!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x588a284ae162bc38!2sSenac%20Campo%20Grande!5e0!3m2!1spt-BR!2sbr!4v1669141608309!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+      </div>
+      <div class="container-fluid">
+      <div class="row social">
+        <div class="col-md-12 text-center">
+          <a href="https://pt-br.facebook.com/" target="_blank"><img src="..imagens/face.png"></a>
+          <a href="https://www.instagram.com/" target="_blank"><img src="..imagens/insta.webp"></a>
+          <a href="https://www.whatsapp.com/?lang=pt_br" target="_blank"><img src="..imagens/wpp.webp"></a>
+        </div>
+      </div>
+      </div>
+
+    </div>
+  </div>
+<script src="script.js"></script>
+  <!-- JavaScript (Opcional) -->
+  <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
+HTML;
